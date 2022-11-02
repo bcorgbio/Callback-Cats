a1aa846b025d0c6cc8b849d19953d72b0f847580
Adam Qu <adamqu@adams-mbp-2.bc.edu>
2022-11-02 16:55:33 -0400

    11/2 4:55

--- adam mod 6.R
+++ adam mod 6.R
@@ -102,13 +102,13 @@ hindwing.pca %>%
   plot_PCA(title = "hindwings")
 
 #COMPARATIVE ANALYSIS
+#answer question 1: do hind/forewings vary in evo rate?
 library(ape)
 
 lep.tree <- ape::read.tree("lep_tree2.tre")
 
 lep.tree <- ladderize(lep.tree)
 plot(lep.tree,cex=0.1)
-
 lep.tree$tip.label <- gsub("_"," ",lep.tree$tip.label)
 basename(names(outs))[1:5]
 
